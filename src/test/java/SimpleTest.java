96ad783b882ddc12c5e7b410dd4f80a98ba306d0
RezwanDigiGate <rezwan.kabir@digigate360.com>
2022-09-24 22:18:33 +0600

    final

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -8,6 +8,20 @@ public class SimpleTest {
         System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        System.out.println("working");
+        System.out.println("Test 1 done");
+    }
+    @Test
+    public void test2(){
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println("Test 2 done");
+    }
+    @Test
+    public void test3(){
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println("Test 3 done");
     }
 }
