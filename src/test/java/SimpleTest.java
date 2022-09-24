361c7cf0cf578bdbd29c3490d7ccaccbb21891ce
RezwanDigiGate <rezwan.kabir@digigate360.com>
2022-09-24 22:20:21 +0600

    final

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -3,21 +3,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
-    @Test
+    @Test(priority = 0)
     public void test(){
         System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         System.out.println("Test 1 done");
     }
-    @Test
+    @Test(priority = 1)
     public void test2(){
         System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         System.out.println("Test 2 done");
     }
-    @Test
+    @Test(priority = 2)
     public void test3(){
         System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
