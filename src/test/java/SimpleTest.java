e600ea7f4d364a3dfa9e0ea730ef0592ed2258a7
RezwanDigiGate <rezwan.kabir@digigate360.com>
2022-09-24 19:08:03 +0600

    p

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
     @Test
     public void test(){
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
         System.out.println("working");
     }
 }
